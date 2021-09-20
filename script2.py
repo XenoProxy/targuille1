1692ffacf3c7f77000b2df7b19f093104062df2f
XenoProxy <iryna.misiuchenko@rozum.com>
2021-09-20 18:39:11 +0300

    script2 (complete)

--- script2.py
+++ script2.py
@@ -1,6 +1,10 @@
+import random
+import string
 from datetime import datetime
 
 for i in range(10):
     curr_time = datetime.today()
+    letters = string.ascii_lowercase
+    rand_string = ''.join(random.choice(letters) for y in range(1000))
     with open(f'test_{curr_time}_{i+1}.txt', 'a+', encoding='utf-8') as file:
-        file.write(curr_time.strftime('%Y-%m-%d-%H-%M-%S') + '\n')
+        file.write(rand_string)

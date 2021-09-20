fb2c035db9760af9c402202bd7d26af428338b80
XenoProxy <iryna.misiuchenko@rozum.com>
2021-09-20 23:03:05 +0300

    script 3 complete

--- script3.py
+++ script3.py
@@ -15,6 +15,9 @@ for proc in psutil.process_iter():
                     os.path.join(path, 'java_pid_list.txt'),
                     'a+', encoding='utf-8') as file:
                 file.write(f'PID: {pid}   NAME: {name}' + '\n')
-            print(name, ': ', pid)
+        with open(
+                os.path.join(path, 'pid_list.txt'),
+                'a+', encoding='utf-8') as file:
+            file.write(f'PID: {pid}   NAME: {name}' + '\n')
     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
         pass

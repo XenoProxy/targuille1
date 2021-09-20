a965e19f39118478dee7b792cc03ed1ef89e50f2
XenoProxy <iryna.misiuchenko@rozum.com>
2021-09-20 23:10:10 +0300

    script 2 cron

--- script2.py
+++ script2.py
@@ -8,3 +8,6 @@ for i in range(10):
     rand_string = ''.join(random.choice(letters) for y in range(1000))
     with open(f'test_{curr_time}_{i+1}.txt', 'a+', encoding='utf-8') as file:
         file.write(rand_string)
+
+# To schedule it to run every 6 hours we can use cron.
+# Example: 0 */6 * * * python ~/targuille1/script1.py
